b9635d73ef307551acb7c64c42e702dd106e07a4
Sebastian Pickl <sebastian.pickl@sz.de>
2019-05-21 16:20:44 +0200

    update Package.swift and project structure

--- Package.swift
+++ Package.swift
@@ -1,13 +1,24 @@
-/**
- This file is part of the Reductio package.
- (c) Sergio Fernández <fdz.sergio@gmail.com>
-
- For the full copyright and license information, please view the LICENSE
- file that was distributed with this source code.
- */
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-  name: "Reductio"
+    name: "Reductio",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Reductio",
+            targets: ["Reductio"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "Reductio", dependencies: []),
+        .testTarget(name: "ReductioTests", dependencies: ["Reductio"])
+    ]
 )
